009bb15c2e183bed68934ae60e012db780c465e9
gkenea <goshukeneatujuba@gmail.com>
2022-06-30 12:54:30 -0700

    The c-program that print with special character with escape character

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
-
+#include <unistd.h>
 /**
  *
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: Always 1 (Success)
  * 
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+	char c[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(2, c, sizeof(c));
 
 	return (1);
 
