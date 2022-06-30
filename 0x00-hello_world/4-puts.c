d416c43bc72cc5a41089898409fe83110e833c7d
gkenea <goshukeneatujuba@gmail.com>
2022-06-30 11:52:15 -0700

    C-program that prints the size of different data types

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
-/* main: Entry point */
+/**
+ * main: To print text with puts
+ *
+ * Return: Always 0 (Success)
+ */
 
-/* Description: To printout by using puts function */
-
-/* Return: Always 0 (Success) */
-
-int main(void) /* Description: To printout by using puts function */
+int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	iputs("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
