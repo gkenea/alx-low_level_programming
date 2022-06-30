8649430a1c296ef6fbece6715636d99cc69f460a
gkenea <goshukeneatujuba@gmail.com>
2022-06-30 10:54:00 -0700

    Printing text format in C program using printf function

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - Print the given format using printf function
  *
  * Return: always 0 (Success)
  */
 
 int main(void)
 i{
-printf("with proper grammar, but the outcome is a piece of art,\n");
-return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
