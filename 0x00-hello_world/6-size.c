649c9bc90ff4db1cb9c0628800e8c5785d0bd8df
gkenea <goshukeneatujuba@gmail.com>
2022-06-30 15:51:22 -0700

    fixing betty error

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,7 +2,6 @@
 
 /**
  * main - Entry point 
- * @parameter: data types
  * Return: Always 0 (Success)
  */
 
@@ -20,6 +19,5 @@ int main(void)
 	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
 	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
 	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(e));
-
-return (0);
+	return (0);
 }
