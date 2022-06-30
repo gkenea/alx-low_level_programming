08c642fea8d2fa97982826d5f11412ed086aee6d
gkenea <goshukeneatujuba@gmail.com>
2022-06-30 13:40:28 -0700

    The c-program that text format with special character using escape charcter

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,17 +1,15 @@
 #include <stdio.h>
 #include <unistd.h>
+
 /**
- *
- * main - Entry point
+ * main: Entry point
  *
  * Return: Always 1 (Success)
- * 
  */
+
 int main(void)
 {
 	char c[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	write(2, c, sizeof(c));
-
 	return (1);
-
 }
