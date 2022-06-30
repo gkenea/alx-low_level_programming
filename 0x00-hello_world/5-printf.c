65a7946a387674299cfa9725fae90a8b25f7fe28
gkenea <goshukeneatujuba@gmail.com>
2022-06-30 06:16:05 -0700

    The C-program that print the given text using printf function

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: always 0 (Success)
+ */
 
 int main(void)
-{
+i{
 printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
