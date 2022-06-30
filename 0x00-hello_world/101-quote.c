ac01540f2270c346f41d3c52a65ceecfbf845004
gkenea <goshukeneatujuba@gmail.com>
2022-06-30 15:26:41 -0700

    error fixing

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
 #include <unistd.h>
 
-/**
- * main: Entry point	
- * Return: Always 1 (Success)
- */
+
 int main(void)
 {
-	char c[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"; /* string member */
+	char c[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
 	write(1, c, 59);
 	return (1);
