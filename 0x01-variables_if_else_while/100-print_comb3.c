b0adc31f7aeaeabc7461bdb57a6d1787f6c730c9
gkenea <goshukeneatujuba@gmail.com>
2022-07-01 06:44:21 -0700

    program that prints all possible different combinations of two digits using putchar function

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,11 +11,11 @@ int main(void)
 	int j = '0';
 
 	while (i <= '9')
-	{	
+	{
 		while (j <= '9')
 		{
 			if (!(i > j || i == j))
-			{		
+			{
 				putchar(i);
 				putchar(j);
 				if (i == '8' && j == '9')
