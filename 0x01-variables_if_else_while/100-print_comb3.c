434ff05de9bc582c2efd49627c54fda74849053c
gkenea <goshukeneatujuba@gmail.com>
2022-07-01 06:40:00 -0700

    a program that prints all possible different combinations of two digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,18 +11,28 @@ int main(void)
 	int j = '0';
 
 	while (i <= '9')
-	{
+	{	
 		while (j <= '9')
-		{											if (!(i > j || i == j))
-													{											putchar(i);
-				putchar(j);																		       if (i == '8' && j == '9')
-			       {
-					putchar('\n');																		}										else
+		{
+			if (!(i > j || i == j))
+			{		
+				putchar(i);
+				putchar(j);
+				if (i == '8' && j == '9')
 				{
-															putchar(',');									putchar(' ');
-														}																			}
-													j++;									}
+					putchar('\n');
+				}
+				else
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			j++;
+		}
 		j = '0';
-		i++;									}
+		i++;
+	}
+
 	return (0);
 }
