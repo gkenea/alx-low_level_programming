671a301b1c777681bfc31338a5e3cd548e776914
gkenea <goshukeneatujuba@gmail.com>
2022-07-01 05:26:33 -0700

    Fixing betty coding style

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -21,5 +21,6 @@ int main(void)
 		printf("Last digit of %i is %i and is 0\n", n, last_digit);
 	else if (last_digit < 6)
 		printf("Last digit of %i is %i and is less than 6 and not 0\n", n, last_digit);
+
 	return (0);
 }
