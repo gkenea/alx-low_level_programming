42f4d2e3714ffd7aa745b97948e9ceca83c75845
gkenea <goshukeneatujuba@gmail.com>
2022-07-01 02:39:01 -0700

    The c-program that prints the lower and upper case letter using putchar function

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the lower and upper case letter
+ *
+ * Return: Always 0 (Success)
+ */
+int main (void)
+{
+	char c = 'a';
+	char d ='A';
+	
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+
+	while (d <= 'Z')
+	{
+		putchar(d);
+		d++;
+	}
+
+	putchar ('\n');
+	return (0);
+}
