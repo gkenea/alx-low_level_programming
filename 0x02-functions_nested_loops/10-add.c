60d43ffc5e5db4abc73a810d955c02ecc7158cbc
gkenea <goshukeneatujuba@gmail.com>
2022-07-05 07:13:43 -0700

    adding number

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - adding numbers
+ * @n1: First number
+ * @n2: Sechond number
+ * Return: sum
+ */
+int add(int n1, int n2)
+{
+	int sum;
+
+	sum = 0;
+	sum = n1 + n2;
+	return (sum);
+}
