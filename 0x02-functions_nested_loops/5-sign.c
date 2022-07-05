c910a24f82bb8e8152038104e2bf37654e45b439
gkenea <goshukeneatujuba@gmail.com>
2022-07-05 09:05:23 -0700

    error of spacing

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,7 +2,7 @@
 
 /**
  * print_sign - check the sign of number
- * @c: number to check
+ * @n: number to check
  * Return: Always 0 or 1
  */
 int print_sign(int n)
@@ -13,13 +13,13 @@ int print_sign(int n)
 		_putchar('+');
 		return (1);
 	}
-	
+
 	else if (n == 0)
 	{
 		_putchar('0');
 		return (0);
 	}
-	
+
 	else
 	{
 		_putchar('-');
