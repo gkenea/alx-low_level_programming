fc49b4bc0cb52181e9c1db0ce7f42628816e461c
gkenea <goshukeneatujuba@gmail.com>
2022-07-05 04:24:33 -0700

    betty style problem fixing

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,6 @@
 #include "main.h"
 
 /**
- *
  * main - Entry point
  *
  * Return: Always 0 (Success)
@@ -10,6 +9,7 @@ int main(void)
 {
 	char c[10] = "_putchar";
 	int i = 0;
+
 	while (i < 8)
 	{
 		_putchar(c[i]);
