1504cde016d35ec83fbe214fe4ba4408c0f70559
gkenea <goshukeneatujuba@gmail.com>
2022-07-14 05:08:55 -0700

    fixing problem

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,28 +2,31 @@
 #include <stdio.h>
 
 /**
- * *cap_string - this is awesome
- * @s: pointer to char params
- * Return: *s
+ * cap_string - Function that capitalizes all words of a string
+ * @s: s is the world to be analyzed
+ *
+ * Return: Always 0.
  */
 
 char *cap_string(char *s)
 {
-	int i, j;
-	char delimeters[] = " \t\n,;.!?\"(){}";
+	int i = 0;
 
-	for (i = 0; s[i] != '\0'; i++)
+	while (s[i] != '\0')
 	{
-		if (s[0] >= 97 && s[0] <= 122)
+		if (s[0] <= 122 && s[0] >= 97)
 		{
 			s[0] = s[0] - 32;
+		}
 
-			for (j = 0; delimeters[j] != '\0'; j++)
+		if (s[i] == 32 || s[i] == 46 || s[i] == '\t' || s[i] == '\n' || s[i] == 44 || s[i] == 59 || s[i] == '!' || s[i] == '?' || s[i] == '(' || s[i] == ')' || s[i] == '{' || s[i] == '}')
+		{
+			if (s[i + 1] <= 122 && s[i + 1] >= 97)
 			{
-				if (s[i] == delimeters[j] && s[i + 1] >= 97 && s[i + 1] <= 122)
-					s[i + 1] = s[i + 1] - 32;
+				s[i + 1] = s[i + 1] - 32;
 			}
 		}
+	i++;
 	}
 	return (s);
 }
