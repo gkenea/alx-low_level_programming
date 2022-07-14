fe9761c6a11c48f20316076b033ac4a084da6586
gkenea <goshukeneatujuba@gmail.com>
2022-07-14 05:12:10 -0700

    m

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -8,12 +8,16 @@
 
 char *string_toupper(char *s)
 {
-	int i;
+	int i = 0;
 
-	for (i = 0; s[i] != '\0'; i++)
+	while (s[i] != '\0')
 	{
-		if (s[i] >= 'a' && s[i] <= 'z
-			s[i] -= 32;
+		if ((s[i] >= 97) && (s[i] <= 122))
+		{
+			s[i] = s[i] - 32;
+		}
+	i++;
 	}
 	return (s);
 }
+
