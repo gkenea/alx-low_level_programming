04a6f30c9a1b6c395397deb58eafc4465e0121de
gkenea <goshukeneatujuba@gmail.com>
2022-07-14 05:43:53 -0700

    message for correction

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * cap_string - Function that capitalizes all words of a string
@@ -10,23 +9,25 @@
 
 char *cap_string(char *s)
 {
-	int i = 0;
+	int count = 0, i;
+	int separators[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
+	if (*(s + count) >= 97 && *(s + count) <= 122)
+		*(s + count) = *(s + count) - 32;
+	count++;
 
-	while (s[i] != '\0')
+	while (*(s + count) != '\0')
 	{
-		if (s[0] <= 122 && s[0] >= 97)
+		for (i = 0; i < 13; i++)
 		{
-			s[0] = s[0] - 32;
-		}
-
-		if (s[i] == 32 || s[i] == 46 || s[i] == '\t' || s[i] == '\n' || s[i] == 44 || s[i] == 59 || s[i] == '!' || s[i] == '?' || s[i] == '(' || s[i] == ')' || s[i] == '{' || s[i] == '}')
-		{
-			if (s[i + 1] <= 122 && s[i + 1] >= 97)
+			if (*(s + count) == separators[i])
 			{
-				s[i + 1] = s[i + 1] - 32;
+				if ((*(s + (count + 1)) >= 97) && (*(s + (count + 1)) <= 122))
+					*(s + (count + 1)) = *(s + (count + 1)) - 32;
+				break;
 			}
 		}
-	i++;
+		count++;
 	}
 	return (s);
 }
+
